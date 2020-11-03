9a007651ee3bef74a8089d0ac614e184c68bdeaa
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-11-03 15:47:16 -0800

    added comments to highlight some topics

--- 01-JS Drum Kit/script.js
+++ 01-JS Drum Kit/script.js
@@ -2,9 +2,11 @@
 function playSound(event) {
     const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
     const key = document.querySelector(`.key[data-key='${event.keyCode}']`);
+    // Only attempt to play a sound if we specified an element with the given data-key attribute
     if (audio) {
         audio.currentTime = 0;
         audio.play();
+        // Add the string to the class list so we know when to transform/reverse
         key.classList.add('playing');
     }
 }
@@ -15,7 +17,9 @@ function removeTransition(event) {
 
 /* ---------- Event Listeners ---------- */
 window.onkeydown = playSound;
+// keys is going to be an array of all the elements with the key class
 const keys = document.querySelectorAll('.key');
 keys.forEach( key => {
+    // add an event listener to each key
     key.addEventListener('transitionend', removeTransition);
 })
