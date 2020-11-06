f501736b13dc5242fdf3fd242088e153d82d8567
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-11-05 18:22:17 -0800

    added ability to click what sound we want to play

--- 01 - JS Drum Kit/script.js
+++ 01 - JS Drum Kit/script.js
@@ -1,3 +1,5 @@
+const keys = document.querySelectorAll('.key');
+
 /* ---------- Event Handlers ---------- */
 function playSound(event) {
     const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
@@ -11,15 +13,24 @@ function playSound(event) {
     }
 }
 
+function buttonClicked(key) {
+    // we access this.dataset.key because we are using the data-* attribute
+    const audio = document.querySelector(`audio[data-key='${this.dataset.key}']`);
+    audio.currentTime = 0;
+    audio.play();
+    this.classList.add('playing');
+}
+
 function removeTransition(event) {
     if (event.propertyName === 'transform') this.classList.remove('playing');
 }
 
 /* ---------- Event Listeners ---------- */
 window.onkeydown = playSound;
+
 // keys is going to be an array of all the elements with the key class
-const keys = document.querySelectorAll('.key');
 keys.forEach( key => {
-    // add an event listener to each key
+    // add an event listener to each key to remove 'playing' from the classList
     key.addEventListener('transitionend', removeTransition);
+    key.addEventListener('click', buttonClicked);
 })
